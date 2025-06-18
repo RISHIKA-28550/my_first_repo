ed6ea00db019b01ccd2c8323c0e2de07bf747a10
RISHIKA-28550 <rishika159btcseai24@gmail.com>
2025-06-18 13:46:38 +0530

    updated the real time weather data using API

--- javascriptProject.js
+++ javascriptProject.js
@@ -1,18 +1,44 @@
-//real time temperature
-
-function showCurrTempandCity(response) {
-  let currTemp = Math.round(response.data.temperature.current);
+function refreshWeatherDetails(response) {
   let mainTempElement = document.querySelector("#current-temp");
+  let currTemp = Math.round(response.data.temperature.current);
   mainTempElement.innerHTML = `${currTemp}`;
 
-  let mainheading = document.querySelector("h1");
-  mainheading.innerHTML = response.data.city;
+  let description = document.querySelector("#weather-description");
+  description.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+
+  let mainCityheading = document.querySelector("h1");
+  mainCityheading.innerHTML = response.data.city;
+
+  //date and time change
+
+  let currDayandTime = document.querySelector("#dayAndTime");
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[now.getDay()];
+  let hour = now.getHours().toString().padStart(2, "0");
+  let minutes = now.getMinutes().toString().padStart(2, "0");
+  currDayandTime.innerHTML = `${day} ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
   let apiKey = "bafoe10ec41e43fbd136804atbea3503";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiURL).then(showCurrTempandCity);
+  axios.get(apiURL).then(refreshWeatherDetails);
 }
 
 function handleSEarchSubmit(event) {
@@ -25,21 +51,3 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSEarchSubmit);
 
 searchCity("New Delhi");
-
-//date and time change
-
-let currDayandTime = document.querySelector("#dayAndTime");
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hour = now.getHours().toString().padStart(2, "0");
-let minutes = now.getMinutes().toString().padStart(2, "0");
-currDayandTime.innerHTML = `${day} ${hour}:${minutes}`;
